2fb751f867ca2ab6e3bf5fb60a1d188b51337689
Nikolay Razlozhki <ngr@peytz.dk>
2019-10-20 23:36:43 +0300

    fixing cross origin header issue

--- zf3/config/config.php
+++ zf3/config/config.php
@@ -13,6 +13,8 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
+    \Zend\Router\ConfigProvider::class,
+    \Zend\Validator\ConfigProvider::class,
     \Zend\HttpHandlerRunner\ConfigProvider::class,
     \Zend\Expressive\Twig\ConfigProvider::class,
     \Zend\Expressive\Router\FastRouteRouter\ConfigProvider::class,
